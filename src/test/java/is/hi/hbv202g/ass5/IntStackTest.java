ceaa2a4ac864b530bee99fe8759605059a0ec0e3
telmajohanns <tej2@hi.is>
2024-02-21 21:44:28 +0000

    Adding question 9, and fixing question 8

--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -16,6 +16,8 @@ public class IntStackTest {
     @Test
     public void testNewStackIsNotFull() {
         assertFalse(s1.isFull());
+        assertTrue(s1.isEmpty());
+
     }
 
     @Test
@@ -24,6 +26,39 @@ public class IntStackTest {
             s1.push(i);
         }
         assertTrue(s1.isFull());
+        assertFalse(s1.isEmpty());
+    }
+
+    @Test
+    public void notAsFull() {
+        for (int i = 0; i < s1.getCapacity() - 1; i++) {
+            s1.push(i);
+        }
+        assertFalse(s1.isFull());
+    }
+
+    @Test
+    public void testPopReturnsPushedValue() {
+        s1.push(1);
+        s1.push(2);
+
+        assertEquals(2, s1.pop());
+        assertEquals(1, s1.pop());
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void popOnEmptyStack() {
+        s1.pop();
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void pushOnFullStack() {
+        int capacity = s1.getCapacity();
+        
+        for (int i = 0; i < capacity; i++) {
+            s1.push(i);
+        }
+        s1.push(1);
     }
 
 }
